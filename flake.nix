0d2606efe7fab887f1dca386edd45f9b5f691b08
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2026-02-07 15:02:41 -0500

    refactor(flake): rename nixpkgs-stable to nixpkgs-master and remove musl preference
    Rename the nixpkgs-stable input to nixpkgs-master to better reflect
    its purpose. Remove zigPreferMusl flag from package builds.

--- flake.nix
+++ flake.nix
@@ -3,13 +3,13 @@
 
   inputs = {
     utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.102";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/fa83fd837f3098e3e678e6cf017b2b36102c7211";
+    nixpkgs-master.url = "github:NixOS/nixpkgs/fa83fd837f3098e3e678e6cf017b2b36102c7211";
     nixpkgs.url = "github:NixOS/nixpkgs/54b154f971b71d260378b284789df6b272b49634";
     zig2nix.url = "github:Cloudef/zig2nix";
   };
 
   outputs =
-    { zig2nix, nixpkgs, nixpkgs-stable, utils, ... }:
+    { zig2nix, nixpkgs, nixpkgs-master, utils, ... }:
     let
       flake-utils = zig2nix.inputs.flake-utils;
     in
@@ -27,12 +27,10 @@
         zmx-package = env.package {
           src = cleanSource ./.;
           zigBuildFlags = [ "-Doptimize=ReleaseSafe" ];
-          zigPreferMusl = true;
         };
         zmx-libvterm = env.package {
           src = cleanSource ./.;
           zigBuildFlags = [ "-Doptimize=ReleaseSafe" "-Dbackend=libvterm" ];
-          zigPreferMusl = true;
           buildInputs = [ pkgs.libvterm-neovim ];
           # Remove ghostty dependency from zon file - not needed for libvterm backend
           postPatch = ''
